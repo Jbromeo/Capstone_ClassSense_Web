ee72d926d898f0effdd6ba5ed13895a97250bea6
King Noel Romeo <romeo.jb7575@gmail.com>
2026-06-26 21:06:08 +0800

    Auto-update Fri 06/26/2026 21:06:08.23

---
+++ auto-push.bat
@@ -0,0 +1,16 @@
+@echo off
+REM auto-push.bat - Commit and push ClassSense to GitHub
+cd /d "C:\xampp\htdocs\ClassSense"
+
+REM Check if a commit message was provided
+if "%1"=="" (
+  set msg=Auto-update %DATE% %TIME%
+) else (
+  set msg=%*
+)
+
+git add -A
+git commit -m "%msg%"
+git push origin main
+
+pause
